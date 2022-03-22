79ad411eea6ac1edf2f349f33660da9d2be87a69
sytnic <sytnic@list.ru>
2022-03-22 16:17:04 +0300

    12.0. Json in Controller on screen

--- app/Http/Controllers/ShowRoomsController.php
+++ app/Http/Controllers/ShowRoomsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 
 class ShowRoomsController extends Controller
 {
@@ -14,6 +16,11 @@ class ShowRoomsController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return response('A listing of rooms', 200);
+        // вывод на экран простейшей строки
+        // return response('A listing of rooms', 200);
+
+        // вывод на экран всех комнат из БД в формате JSON
+        $rooms = DB::table('rooms')->get();
+        return response()->json($rooms);
     }
 }
