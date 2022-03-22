d6ee0f5600f44ebc4880c285f7e402665cfc0464
sytnic <sytnic@list.ru>
2022-03-22 11:35:55 +0300

    10. Creating routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// работает по адресу /api/test
+Route::get('/test', function() { return "Hello World"; });
